ea22756b3d455db085a9073dccb85d608aa38cca
sharanumesta-leet <sharanumesta@gmail.com>
2025-11-09 17:52:26 +0530

    Time: 2 ms (93.74%) | Memory: 72.5 MB (5.37%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        for (int num : bills) {
+            if (num == 5)
+                five++;
+            else if (num == 10) {
+                if (five == 0)
+                    return false;
+                five--;
+                ten++;
+            } else if (num == 20) {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3)
+                    five -= 3;
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+}
