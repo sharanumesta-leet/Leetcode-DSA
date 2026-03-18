4a9508d0d0de62997599ce63ebc487fec5177272
sharanumesta-leet <sharanumesta@gmail.com>
2026-03-18 21:55:23 +0530

    Time: 48 ms (71.13%) | Memory: 48.3 MB (72.92%) - LeetSync

---
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : banned)
+            set.add(num);
+        int sum = 0;
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (set.contains(i))
+                continue;
+            if (sum + i > maxSum)
+                break;
+            sum += i;
+            count++;
+        }
+        return count;
+    }
+}
