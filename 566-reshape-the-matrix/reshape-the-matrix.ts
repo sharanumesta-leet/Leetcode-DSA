21d90cce4254dbe65733270cb9da7181b7773197
sharanumesta-leet <sharanumesta@gmail.com>
2026-06-12 21:48:53 +0530

    Time: 1 ms (87.10%) | Memory: 59.7 MB (74.19%) - LeetSync

---
+++ 566-reshape-the-matrix/reshape-the-matrix.ts
@@ -0,0 +1,17 @@
+function matrixReshape(mat: number[][], r: number, c: number): number[][] {
+    const m = mat.length;
+    const n = mat[0].length;
+    if (m * n !== r * c) return mat;
+
+    const reshaped = Array.from({ length: r }, () => Array(c).fill(0));
+    let count = 0;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            reshaped[Math.floor(count / c)][count % c] = mat[i][j];
+            count++;
+        }
+    }
+
+    return reshaped;
+};
