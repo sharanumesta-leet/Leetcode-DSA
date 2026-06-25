be2197d39a86e8d53e75f4ce19a5581c2dca4398
sharanumesta-leet <sharanumesta@gmail.com>
2026-06-25 21:59:02 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (73.08%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.ts
@@ -0,0 +1,26 @@
+function isPathCrossing(path: string): boolean {
+    let x = 0, y = 0;
+    let visited = new Set<string>();
+    visited.add("0,0");
+
+    for (let direction of path) {
+        if (direction === 'E') {
+            x++;
+        } else if (direction === 'W') {
+            x--;
+        } else if (direction === 'N') {
+            y++;
+        } else if (direction === 'S') {
+            y--;
+        }
+
+        let currentPos = `${x},${y}`;
+        if (visited.has(currentPos)) {
+            return true;
+        }
+
+        visited.add(currentPos);
+    }
+
+    return false;
+};
