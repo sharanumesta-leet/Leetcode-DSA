13da6995275413a5c9f762a50a5ca7a16d95a66d
sharanumesta-leet <sharanumesta@gmail.com>
2026-07-12 22:29:23 +0530

    Time: 1 ms (84.38%) | Memory: 56.3 MB (90.63%) - LeetSync

---
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.ts
@@ -0,0 +1,8 @@
+function targetIndices(nums: number[], target: number): number[] {
+    nums.sort((a, b) => a - b);
+    let ans = [];
+    for (let i = 0; i < nums.length; i++)
+        if (nums[i] === target)
+            ans.push(i);
+    return ans;
+};
